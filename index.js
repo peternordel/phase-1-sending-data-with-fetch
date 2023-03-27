cb38f7b5729da18ad022b2eca949bd1edab21c43
Peter Nordel <peternordel@gmail.com>
2023-03-26 19:39:39 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function submitData (name, email) {
+    const bodyElement = document.querySelector('body');
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            'name': name,
+            'email': email,
+        }),
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        //retrieves new id value and appends it to the DOM
+        bodyElement.append(data.id);
+    })
+    .catch(error => {
+        //handles a failed POST request using catch, appends the error message to the DOM
+        bodyElement.append("Unauthorized Access");
+    })
+}
